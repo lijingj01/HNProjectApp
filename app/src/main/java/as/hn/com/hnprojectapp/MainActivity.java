af5be8c3d83be26f7b76102de0c548e14d513288
lijingj <lijingj@live.com>
2018-08-24 23:20:17 +0800

    完成计划管理功能，并编译发布到了手机正常运行

--- app/src/main/java/as/hn/com/hnprojectapp/MainActivity.java
+++ app/src/main/java/as/hn/com/hnprojectapp/MainActivity.java
@@ -10,7 +10,7 @@ import android.view.MenuItem;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends MyActivityBase {
 
     @BindView(R.id.toolbar)
     Toolbar toolbar;
--- app/src/main/java/as/hn/com/hnprojectapp/PlanVIewActivity.java
+++ app/src/main/java/as/hn/com/hnprojectapp/PlanVIewActivity.java
@@ -8,6 +8,8 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -21,7 +23,7 @@ import java.util.List;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
-public class PlanVIewActivity extends AppCompatActivity {
+public class PlanVIewActivity extends MyActivityBase {
 
     private ProjectPlanEntity entity;
     private List<PlanPercentageEntity> perList;
@@ -95,6 +97,28 @@ public class PlanVIewActivity extends AppCompatActivity {
         mPercentageBar.setmTxtHint2("工作量");
         //endregion
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+//        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        int id = item.getItemId();
+
+        if (id == android.R.id.home) {
+            //打开主页
+            //打开列表页面
+            Intent intent = new Intent(PlanVIewActivity.this, PlanListActivity.class);
+            startActivity(intent);
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
 }
